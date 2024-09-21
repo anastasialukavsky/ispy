5cecd5e127aaa73ddf0449f9b833dde80bca851a
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-21 16:07:55 -0400

    Add colorVariant prop

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,18 +1,38 @@
-import React, { ReactNode } from 'react';
-
+import  { ReactNode } from 'react';
 
 type ButtonProps = {
   children: ReactNode;
   onClick: () => void;
   className?: string;
-}
-
-export const Button = ({ children, onClick, className }: ButtonProps) => {
-  return (
-    <button onClick={onClick} className={`px-4 py-2 ${className}`}>
-      {children}
-    </button>
-  );
+  colorVariant?: 'light' | 'dark';
 };
 
+export const Button = ({
+  onClick,
+  className,
+  children,
+  colorVariant,
+}: ButtonProps) => {
+  return (
+    <div className={`relative group ${className}`}>
+      <button
+        onClick={onClick}
+        className={`${
+          colorVariant === 'dark'
+            ? 'border-primary-dark-gray'
+            : 'border-primary-light-fill'
+        } w-full h-10 font-medium relative z-10 border-2`}
+      >
+        {children}
+      </button>
+      <span
+        className={`absolute left-0 top-0 z-0 h-full w-full -rotate-3 border-2 ${
+          colorVariant === 'dark'
+            ? 'border-primary-dark-gray'
+            : 'border-primary-light-fill'
+        } transition-transform duration-300 ease-in-out group-hover:rotate-0`}
+      ></span>
+    </div>
+  );
+};
 
