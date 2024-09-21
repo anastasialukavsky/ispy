85772e21a10453944ca0b5c1d8eb84b6079b69c0
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-21 17:21:28 -0400

    Add enable.disable styles for toolbox

--- src/algos/toolbox/Toolbox.tsx
+++ src/algos/toolbox/Toolbox.tsx
@@ -4,33 +4,40 @@ import { Button } from '../../UI/';
 type ToolboxProps = {
   setSelectedAlgo: Dispatch<SetStateAction<string | null>>;
   setDisplayMetadata: React.Dispatch<React.SetStateAction<boolean>>;
+  setEnableButton: React.Dispatch<React.SetStateAction<boolean>>;
+  enableButton: boolean;
 };
 
 export default function Toolbox({
   setSelectedAlgo,
   setDisplayMetadata,
+  enableButton,
+  setEnableButton,
 }: ToolboxProps) {
+  const buttonEnabledStyles = ' mb-4 bg-button-unabled-fill';
+  const buttonDisabledStyles =
+    'mb-4 text-toolbox-gray mb-4 bg-button-disabled-fill';
   const buttonStyles = 'mb-4';
 
   return (
-    <section className='flex flex-col translate-y-full justify-center'>
+    <section className='flex flex-col translate-y-full justify-center font-bench text-lg tracking-wide'>
       <Button
         onClick={() => {
           setSelectedAlgo('ELA');
           setDisplayMetadata(false);
         }}
-        colorVariant='dark'
-        className={buttonStyles}
+        colorVariant={enableButton ? 'light' : 'dark'}
+        className={enableButton ? buttonEnabledStyles : buttonDisabledStyles}
       >
-        ELA
+        Error Level Analysis
       </Button>
       <Button
         onClick={() => {
           setSelectedAlgo('Noise');
           setDisplayMetadata(false);
         }}
-        colorVariant='dark'
-        className={buttonStyles}
+        colorVariant={enableButton ? 'light' : 'dark'}
+        className={enableButton ? buttonEnabledStyles : buttonDisabledStyles}
       >
         Noise Analysis
       </Button>
@@ -48,8 +55,8 @@ export default function Toolbox({
           setSelectedAlgo('Metadata');
           setDisplayMetadata(true);
         }}
-        colorVariant='dark'
-        className={buttonStyles}
+        colorVariant={enableButton ? 'light' : 'dark'}
+        className={enableButton ? buttonEnabledStyles : buttonDisabledStyles}
       >
         Metadata
       </Button>
@@ -58,15 +65,15 @@ export default function Toolbox({
           setSelectedAlgo('Weather');
           setDisplayMetadata(false);
         }}
-        colorVariant='dark'
-        className={buttonStyles}
+        colorVariant={enableButton ? 'light' : 'dark'}
+        className={enableButton ? buttonEnabledStyles : buttonDisabledStyles}
       >
         Weather
       </Button>
       <Button
         onClick={() => setSelectedAlgo('All')}
-        colorVariant='dark'
-        className={buttonStyles}
+        colorVariant={enableButton ? 'light' : 'dark'}
+        className={enableButton ? buttonEnabledStyles : buttonDisabledStyles}
       >
         Apply All
       </Button>
