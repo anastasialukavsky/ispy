4804fdd3871b8c6a825245aaf4be97ad43773d55
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-22 12:19:22 -0400

    Change logo styles

--- src/UI/Logo.tsx
+++ src/UI/Logo.tsx
@@ -7,8 +7,8 @@ export const Logo = () =>{
     navigate('/')
   }
   return (
-    <div className='block h-20 w-20'>
-      <img src="public/assets/loggo.png" alt="" onClick={handleLogoClick}/>
+    <div className='block w-14'>
+      <img src="public/assets/logggo.png" alt="" onClick={handleLogoClick}/>
     </div>
   )
 }
