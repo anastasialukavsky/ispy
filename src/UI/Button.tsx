4ff3102e22c860cb70c5091cd96bd592d9ee103a
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-22 12:26:24 -0400

    Add article content, change btn border

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -29,7 +29,7 @@ export const Button = ({
         onClick={!disabled ? onClick : undefined}
         className={`${
           colorVariant === 'dark' ? 'border-black' : 'border-primary-light-fill'
-        } w-full h-14 font-medium relative z-10 border-2 ${
+        } w-full h-14 font-medium relative z-10 border ${
           disabled ? 'cursor-not-allowed' : ''
         }`}
         disabled={disabled}
@@ -42,7 +42,7 @@ export const Button = ({
         </div>
       )}
       <span
-        className={`absolute left-0 top-0 z-0 h-full w-full -rotate-3 border-2 ${
+        className={`absolute left-0 top-0 z-0 h-full w-full -rotate-3 border ${
           colorVariant === 'dark' ? 'border-black' : 'border-primary-light-fill'
         } ${spanBgVariant} transition-transform duration-300 ease-in-out group-hover:rotate-0`}
       ></span>
--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Logo } from '../UI';
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+
+  // Function to determine the background color based on route
+  const getNavbarColor = () => {
+    switch (location.pathname) {
+      case '/':
+        return 'bg-primary-light-fill'; // Light background for landing page
+      default:
+        return 'bg-primary-dark-gray'; // Dark background for other pages
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Combine scroll and route-based background color
+  const getFinalNavbarColor = () => {
+    const routeColor = getNavbarColor();
+    return isScrolled
+      ? 'bg-primary-dark-gray/20 '
+      : routeColor;
+  };
+
+  // Function to determine NavLink styles based on route
+  const getNavlinkColor = () => {
+    // On landing page, set NavLink text to black
+    if (location.pathname === '/') {
+      return 'text-black hover:underline hover:text-black';
+    }
+    // On other pages, set NavLink text to white
+    return 'text-white hover:underline hover:text-white';
+  };
+
   return (
-    <nav className='sticky top-0 z-50 flex h-16 w-full items-center justify-between bg-white text-xl font-bench'>
+    <nav
+      className={`sticky top-0 z-50 flex h-16 w-full items-center justify-between transition-colors duration-300 ${getFinalNavbarColor()} text-xl font-bench`}
+    >
       {/* Left Section: Logo */}
       <div className='pl-10'>
         <Logo />
@@ -13,29 +61,29 @@ export default function Navbar() {
       {/* Right Section: Navigation Links */}
       <ul className='flex gap-6 pr-10'>
         <li>
-          <NavLink to='/' className='hover:underline'>
+          <NavLink to='/' className={getNavlinkColor()}>
             home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/upload' className='hover:underline'>
+          <NavLink to='/upload' className={getNavlinkColor()}>
             upload image
           </NavLink>
         </li>
         <li>
-          <NavLink to='/about' className='hover:underline'>
+          <NavLink to='/about' className={getNavlinkColor()}>
             about
           </NavLink>
         </li>
         <li>
-          <NavLink to='/account' className='hover:underline'>
+          <NavLink to='/account' className={getNavlinkColor()}>
             account
           </NavLink>
         </li>
       </ul>
 
       {/* Bottom Border */}
-      {/* <div className='absolute bottom-0 right-0 h-[2px] w-4/12 bg-black'></div> */}
+      {/* <div className='absolute bottom-0 right-0 h-[1px] w-full bg-black'></div> */}
     </nav>
   );
 }
