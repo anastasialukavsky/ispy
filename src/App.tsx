e6011d1c1bf4015b06fc7e3ec93405467b8aa73a
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-22 19:18:51 -0400

    Install lenis, gsap, add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,21 @@ import About from './nav/nav-components/about/About';
 import Account from './nav/nav-components/Account';
 import ImageUploader from './nav/nav-components/ImageUploader';
 import './global.css';
-import ELADetails from './nav/nav-components/about/ELADetails';
+import Lenis from 'lenis';
 
 function App() {
+  const lenis = new Lenis();
+
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
+
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
     <OpenCvProvider>
       <Router>
