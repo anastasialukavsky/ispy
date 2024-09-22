a947c86e0fcaa7f9db2f580192dcd2768dc17171
Anastasia Lukavsky <92340133+anastasialukavsky@users.noreply.github.com>
2024-09-21 20:29:45 -0400

    Add wildcart about routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Landing from './landing/Landing';
 import Navbar from './nav/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import About from './nav/nav-components/About';
+import About from './nav/nav-components/about/About';
 import Account from './nav/nav-components/Account';
 import ImageUploader from './nav/nav-components/ImageUploader';
 import './global.css';
+import ELADetails from './nav/nav-components/about/ELADetails';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/about' element={<About />} />
+          <Route path='/about/*' element={<About />} />
           <Route path='/account' element={<Account />} />
           <Route path='/upload' element={<ImageUploader />} />
         </Routes>
